c86df49491552588ca1ad9cbd2526cb8ed255231
francis1641 <francis1641@users.noreply.github.com>
2020-12-14 10:17:29 +0100

    Tema 6
    Ejercicios de AJAX

---
+++ Tema 6/index2.js
@@ -0,0 +1,10 @@
+function localDoc(){
+    var xhttp =new XMLHttpRequest();
+    xhttp.onreadystatechange= function(){
+    if(this.readyState == 4 && this.status== 200){
+        document.getElementById("demo").innerHTML+= this.responseText;
+    }
+    };
+    xhttp.open("GET", "1.txt", true);
+    xhttp.send();
+}
