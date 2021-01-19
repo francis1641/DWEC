4f852e391bf25af7b86c37e8df79c945f222801c
francis1641 <francis1641@users.noreply.github.com>
2021-01-19 14:20:18 +0100

    Cambios en las tareas
    No me hace bien al borrar las tareas completadas, siempre se me queda una sin borrar le tengo que dar dos veces

--- Tema 7/index.js
+++ Tema 7/index.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#miAplicacion',
     data: {
       nuevoRecordatorio: "",
-      listaRecordatorio: []
+      listaRecordatorio: [],
+      isButtomDisabled: true
     },
 
     methods:{
@@ -16,11 +17,43 @@ var app = new Vue({
         );
         this.nuevoRecordatorio="";
         },
-        borrarTodo: function(){
-            
-        },
         cambiarEstadoTarea: function(posicion){
-            this.listaRecordatorio.splice(posicion,1);
+            //this.listaRecordatorio.splice(posicion,1);
+            if(this.listaRecordatorio[posicion].completado)
+                this.listaRecordatorio[posicion].completado=false;
+            else this.listaRecordatorio[posicion].completado=true;
+
+        },
+
+        habilitarBoton: function(){
+            if(this.nuevoRecordatorio.length>0)
+                this.isButtomDisabled=false;
+            else 
+                this.isButtomDisabled=true;
+        },
+        quitarCompletadas: function(){
+            for(let i=0; i<this.listaRecordatorio.length; i++){
+                if(this.listaRecordatorio[i].completado)
+                this.listaRecordatorio.splice(i,1);
+            }
         }
+    },
+    computed: {
+        totalTareas: function(){
+           return this.listaRecordatorio.length
+        },
+
+        totalPendientes: function(){
+            let total=0;
+
+            for(let i=0; i<this.listaRecordatorio.length; i++){
+                if(!this.listaRecordatorio[i].completado)
+                total+=1;
+            }
+
+            return total;
+        },
+
+
     }
   })
