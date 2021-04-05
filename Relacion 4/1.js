8df64d3a4dbcc9946b84f5d122ad07313d2ecf21
francis1641 <73337211+francis1641@users.noreply.github.com>
2021-04-05 17:12:34 +0200

    Ejercicio 1 sin resolver

---
+++ Relacion 4/1.js
@@ -0,0 +1,8 @@
+function fondoColor(color){
+    //document.getElementById(color);
+    document.body.style.backgroundColor= color;
+}
+
+function fondoBlanco(){
+    document.body.style.backgroundColor= "white";
+}
