1dc9d1b8d2422acdb81464a1c9a06a1c5640c643
francis1641 <73337211+francis1641@users.noreply.github.com>
2021-04-26 17:04:28 +0200

    Ejercicio Calculadora

---
+++ Relacion 6/Calculadora.js
@@ -0,0 +1,17 @@
+function loadDoc() {
+    evento=window.event;
+    id=evento.target.id;
+    valor1=document.getElementById("primero").value;
+    valor2=document.getElementById("segundo").value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        misDatos= JSON.parse(this.responseText);
+        console.log(misDatos.resultado);
+      }
+    };
+    xhttp.open("GET", "Calculadora.php?mivariable="+id+"&valor1="+valor1+"&valor2="+valor2, true);
+    xhttp.send();
+}
+
+//"Calculadora.php?mivariable="+id+"&valor1="+valor1+"&valor2="+valor2
